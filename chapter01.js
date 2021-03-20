2ec41b38672629b1084241e007431542585ed90a
dino <oheadid@gmail.com>
2021-03-20 14:05:12 +0900

    chapter01 네번째

--- chapter01.js
+++ chapter01.js
@@ -22,25 +22,42 @@ const invoices = {
     ]
 };
 
-function statement(invoice, plays) {
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-    const format = new Intl.NumberFormat("en-US", {
-        style: "currency", currency: "USD", minimumFractionDigits: 2
-    }).format;
+function statement(invoices, plays) {
+    let result = `청구 내역 (고객명: ${invoices.customer})\n`;
 
-    for(let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
-        
+    for(let perf of invoices.performances) {
         //청구 내역을 출력한다.
-        result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석) \n`;
-        totalAmount += amountFor(perf);
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석) \n`;
     }
-    //청구 내역을 출력한다.
-    result += `총액: ${format(totalAmount/100)}\n`;
-    result += `적립 포인트: ${volumeCredits}점\n`;
+
+   //청구 내역을 출력한다.
+    result += `총액: ${usd(getTotalAmount())}\n`;
+    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
     return result;
+}
+
+function getTotalAmount() {
+    let result = 0;
+
+    for(let perf of invoices.performances) {
+        result += amountFor(perf);
+    }
+
+    return result;
+}
+
+function totalVolumeCredits() {
+    let result = 0;
+    for(let perf of invoices.performances) {
+        result += volumeCreditsFor(perf);
+    }
+    return result;
+}
+
+function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+        style: "currency", currency: "USD", minimumFractionDigits: 2 
+    }).format(aNumber/100);
 }
 
 function playFor(aPerfomance) {
