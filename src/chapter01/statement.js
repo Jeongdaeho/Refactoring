3c7790c4d7f9cb56cde2b018220e4ce4f9367cc5
Daeho Jeong <oheadid@gmail.com>
2022-05-25 23:34:21 +0900

    renderPlainText 함수안에 amountFor 함수 관련 부분을 중간데이터 사용으로 변경한다.

--- src/chapter01/statement.js
+++ src/chapter01/statement.js
@@ -9,28 +9,14 @@ function statement(invoice, plays) {
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance); //얕은 복사
-    result.play = playFor(aPerformance);
+    result.play = playFor(result);
+    result.amount = amountFor(result);
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
-}
-
-function renderPlainText(data) {
-  let result = `Statement for ${data.customer}\n`;
-
-  for (let perf of data.performances) {
-    // 청구 내역을 출력한다.
-    result += `  ${perf.play.name}: ${usd(amountFor(perf))} (${
-      perf.audience
-    } seats)\n`;
-  }
-
-  result += `Amount owed is ${usd(totalAmount())}\n`;
-  result += `You earned ${totalVolumeCredits()} credits\n`;
-  return result;
 
   function amountFor(aPerformance) {
     let result = 0;
@@ -55,6 +41,21 @@ function renderPlainText(data) {
 
     return result;
   }
+}
+
+function renderPlainText(data) {
+  let result = `Statement for ${data.customer}\n`;
+
+  for (let perf of data.performances) {
+    // 청구 내역을 출력한다.
+    result += `  ${perf.play.name}: ${usd(perf.amount)} (${
+      perf.audience
+    } seats)\n`;
+  }
+
+  result += `Amount owed is ${usd(totalAmount())}\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
+  return result;
 
   function volumeCreditFor(aPerformance) {
     let result = 0;
@@ -87,7 +88,7 @@ function renderPlainText(data) {
     let result = 0;
 
     for (let perf of data.performances) {
-      result += amountFor(perf);
+      result += perf.amount;
     }
     return result;
   }
