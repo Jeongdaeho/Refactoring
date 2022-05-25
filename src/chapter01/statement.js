362f3c0dc4b180941797e194239ae4c58f7e5e1f
Daeho Jeong <oheadid@gmail.com>
2022-05-26 00:12:48 +0900

    반복문을 파이프 라인으로수정

--- src/chapter01/statement.js
+++ src/chapter01/statement.js
@@ -55,22 +55,11 @@ function statement(invoice, plays) {
   }
 
   function totalVolumeCredits(data) {
-    let result = 0;
-
-    for (let perf of data.performances) {
-      // 포인트를 적립한다.
-      result += perf.volumeCredits;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
 
   function totalAmount(data) {
-    let result = 0;
-
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.amount, 0);
   }
 }
 
