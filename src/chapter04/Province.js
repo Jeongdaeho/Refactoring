9d76cb09697ae0d1ef8536c8523b507124447c05
Daeho Jeong <oheadid@gmail.com>
2022-05-27 21:50:13 +0900

    chp2 Province getter setter 추가

--- src/chapter04/Province.js
+++ src/chapter04/Province.js
@@ -14,4 +14,36 @@ class Province {
     this._producers.push(arg);
     this._totalProduction += arg.production;
   }
+
+  get name() {
+    return this._name;
+  }
+
+  get producers() {
+    return this._producers;
+  }
+
+  get totalProduction() {
+    return this._totalProduction;
+  }
+
+  set totalProduction(arg) {
+    this._producers = arg;
+  }
+
+  get demand() {
+    return this._demand;
+  }
+
+  set demand(arg) {
+    return (this._demand = parseInt(arg));
+  }
+
+  get price() {
+    return this._price;
+  }
+
+  set price(arg) {
+    this._price = parseInt(arg);
+  }
 }
