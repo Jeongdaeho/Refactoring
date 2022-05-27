3b25f675ce694b761561144d3e45774e9efc6fcf
Daeho Jeong <oheadid@gmail.com>
2022-05-28 00:34:08 +0900

    chp4 빈문자열 체크

--- test/chapter04.test.js
+++ test/chapter04.test.js
@@ -15,7 +15,7 @@ function sampleProvinceData() {
   };
 }
 
-describe("statement", () => {
+describe("province", () => {
   let asia;
   beforeEach(() => {
     asia = new Province(sampleProvinceData());
@@ -33,6 +33,24 @@ describe("statement", () => {
     asia.producers[0].production = 20;
     expect(asia.shortfall).equal(-6);
     expect(asia.profit).equal(292);
+  });
+
+  it("zero demand", () => {
+    asia.demand = 0;
+    expect(asia.shortfall).equal(-25);
+    expect(asia.profit).equal(0);
+  });
+
+  it("negative demand", () => {
+    asia.demand = -1;
+    expect(asia.shortfall).equal(-26);
+    expect(asia.profit).equal(-10);
+  });
+
+  it("empty string demand", () => {
+    asia.demand = "";
+    expect(asia.shortfall).NaN;
+    expect(asia.profit).NaN;
   });
 });
 
@@ -54,31 +72,5 @@ describe("no producers", function () {
 
   it("profit", function () {
     expect(noProducers.profit).equal(0);
-  });
-});
-
-describe("zero demand", function () {
-  let asia;
-  beforeEach(() => {
-    asia = new Province(sampleProvinceData());
-  });
-
-  it("zero demand", function () {
-    asia.demand = 0;
-    expect(asia.shortfall).equal(-25);
-    expect(asia.profit).equal(0);
-  });
-});
-
-describe("negative demand", function () {
-  let asia;
-  beforeEach(() => {
-    asia = new Province(sampleProvinceData());
-  });
-
-  it("zero demand", function () {
-    asia.demand = -1;
-    expect(asia.shortfall).equal(-26);
-    expect(asia.profit).equal(-10);
   });
 });
