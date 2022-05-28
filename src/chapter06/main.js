d2cdd63923fdc89d0e24610ec06234fb0451772e
Daeho Jeong <oheadid@gmail.com>
2022-05-28 20:15:33 +0900

    chp6-1 완료

--- src/chapter06/main.js
+++ src/chapter06/main.js
@@ -1,28 +1,35 @@
-export function printOwing(invoice, console, clock) {
-	let outstanding = 0;
-
-	printBanner();
-	
-	// 미해결 채무
-	for (const o of invoice.orders) {
-			outstanding += o.amount;
-	}
-
-	// 마감일(dueDate)을 기록한다.
-	const today = clock.today;
-	invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
-
-	printDetail(invoice, outstanding);
+export function printOwing(invoice) {
+  printBanner();
+  const outstanding = calculateOutstanding(invoice);
+	recodeDueDate(invoice);
+	printDetail(invoice, outstanding)
 }
 
 function printBanner() {
-	console.log("***********************");
-	console.log("****** 고객 채무 ******");
-	console.log("***********************");
+  console.log("***********************");
+  console.log("****** 고객 채무 ******");
+  console.log("***********************");
+}
+
+function recodeDueDate(invoice) {
+	const today = clock.today;
+	invoice.dueDate = new Date(
+		today.getFullYear(),
+		today.getMonth(),
+		today.getDate() + 30
+	);
 }
 
 function printDetail(invoice, outstanding) {
-	console.log(`name: ${invoice.customer}`);
-	console.log(`amount: ${outstanding}`);
-	console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+  console.log(`name: ${invoice.customer}`);
+  console.log(`amount: ${outstanding}`);
+  console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+}
+
+function calculateOutstanding(invoice) {
+  let result = 0;
+  for (const o of invoice.orders) {
+    result += o.amount;
+  }
+	return result;
 }
