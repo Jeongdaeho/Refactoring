1918cab42227191ceeea2b068901fe03a10d2dee
Daeho Jeong <oheadid@gmail.com>
2022-06-02 16:09:04 +0900

    6-3 init

--- test/chapter06.test.js
+++ test/chapter06.test.js
@@ -1,7 +1,8 @@
 import { expect } from "chai";
 import { getRating } from "../src/chapter06/rating.js";
+import { price } from "../src/chapter06/extractVariable";
 
-describe('chp6', function () {
+describe('chp6-2', function () {
   it("chp6-2", function () {
     expect(getRating({
 			numberOfLateDeliveries: 10
@@ -9,3 +10,11 @@ describe('chp6', function () {
   });
 });
 
+describe('chp6-3', function () {
+  it("price", function () {
+    expect(price({
+      quantity: 500,
+      itemPrice: 55
+		})).equal(27600);
+  });
+});
