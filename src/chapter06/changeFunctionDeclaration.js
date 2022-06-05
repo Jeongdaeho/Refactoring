4fcc2984cf85f909812e87395bfc26842b46dc35
Daeho Jeong <oheadid@gmail.com>
2022-06-05 17:05:33 +0900

    6-3. xxNewEngland 함수 추출하기

--- src/chapter06/changeFunctionDeclaration.js
+++ src/chapter06/changeFunctionDeclaration.js
@@ -2,5 +2,9 @@ export {inNewEngland};
 
 function inNewEngland(aCustomer) {
 	const stateCode = aCustomer.address.state;
+	return xxNewEngland(stateCode);
+}
+
+function xxNewEngland(stateCode) {
 	return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(stateCode);
 }
